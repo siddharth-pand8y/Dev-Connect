b2aeb46d6a0c034f6d26f0a474057c9034f133b1
Siddharth Pandey <siddharth.pand8y@gmail.com>
2018-12-23 21:56:39 +0530

    DeprecationWarning: current URL string parser is deprecated issue fixed

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ const db = require('./config/keys').mongoURI;
 
 //Connecting to MongoDB
 mongoose
-  .connect(db)
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
