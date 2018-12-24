227dc7a25c6908de755b683417da780149bc37e1
Siddharth Pandey <siddharth.pand8y@gmail.com>
2018-12-24 17:33:51 +0530

    Creating JWT

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 //making this object available outside this file
 module.exports = {
-  mongoURI: 'mongodb://usr-prarachn:Dett8Dawa@ds131711.mlab.com:31711/mern1'
+  mongoURI: 'mongodb://usr-prarachn:Dett8Dawa@ds131711.mlab.com:31711/mern1',
+  secretOrKey: 'secret'
 };
--- routes/API/users.js
+++ routes/API/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 //Load User model
 const User = require('../../models/User');
@@ -63,7 +65,19 @@ router.post('/login', (req, res) => {
     //Check password
     bcrypt.compare(password, user.password).then(isMatch => {
       if (isMatch) {
-        res.json({ msg: 'Success' });
+        //User Match
+        const payload = { id: user.id, name: user.name, avatar: user.avatar };
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: 'Bearer ' + token
+            });
+          }
+        );
       } else {
         return res.status(400).json({ password: 'Password Incorrect' });
       }
