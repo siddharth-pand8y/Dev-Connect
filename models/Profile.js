93f6692ccdc27ae12e44367249ebe4f7aade31f0
Siddharth Pandey <siddharth.pand8y@gmail.com>
2018-12-27 01:31:05 +0530

    Current User Profile Route

--- models/Profile.js
+++ models/Profile.js
@@ -35,87 +35,85 @@ const ProfileSchema = new Schema({
   githubusername: {
     type: String
   },
-  experience: [ 
-     {
-         title: {
-            type: String,
-            required: true
-         },
-         company: {
-            type: String,
-            required: true
-         },
-         location: {
-            type: String
-         },
-         from: {
-            type: Date,
-            required: true
-         },
-         to: {
-            type: Date
-         },
-         current: {
-            type: Boolean,
-            default: false
-         },
-         description: {
-            type: String
-         }
+  experience: [
+    {
+      title: {
+        type: String,
+        required: true
+      },
+      company: {
+        type: String,
+        required: true
+      },
+      location: {
+        type: String
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
       }
+    }
   ],
   education: [
-     {
-         school: {
-            type: String,
-            required: true
-         },
-         degree: {
-            type: String,
-            required: true
-         },
-         fieldofstudy: {
-            type: String,
-            required: true
-         },
-         from: {
-            type: Date,
-            required: true
-         },
-         to: {
-            type: Date
-         },
-         current: {
-            type: Boolean,
-            default: false
-         },
-         description: {
-            type: String
-         }
+    {
+      school: {
+        type: String,
+        required: true
+      },
+      degree: {
+        type: String,
+        required: true
+      },
+      fieldofstudy: {
+        type: String,
+        required: true
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
       }
-   ],
-   social: {
-      youtube: {
-         type: String
-      }
-      facebook: {
-         type: String
-      }
-      twitter: {
-         type: String
-      }
-      instagram: {
-         type: String
-      }
-      linkedin: {
-         type: String
-   }
-},
-date: {
-   type: Date,
-   default: Date.now
-}
-
-
+    }
+  ],
+  social: {
+    youtube: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    instagram: {
+      type: String
+    },
+    linkedin: {
+      type: String
+    }
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
-module.exports = Profile = mongoose.model('profile', ProfileSchema)
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
